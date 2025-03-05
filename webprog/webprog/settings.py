048e12127f870cdb8d8410c8944a3e6e0f71e7bb
Mukhammed <just.mukhammed@gmail.com>
2025-03-06 00:54:54 +0500

    Первый и единственный коммит

--- webprog/webprog/settings.py
+++ webprog/webprog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-asdq3-ouihjasdfhui23@$%@$Rqef23ewFDAS_314EWDF'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boogiedance.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
